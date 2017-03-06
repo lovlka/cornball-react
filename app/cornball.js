af31e178ccbb53e42a6a4a78386b42c61fba2787
Victor Stodell <victor@stodell.se>
2017-03-06 20:31:27 +0100

    Added React intl

--- app/cornball.js
+++ app/cornball.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router'
 import { Provider } from 'react-redux';
+import { IntlProvider } from 'react-intl';
 import configureStore from './configure-store';
 
 import Root from './components/root'
@@ -12,13 +13,15 @@ import About from './components/about'
 document.addEventListener('DOMContentLoaded', () => {
    ReactDOM.render(
       <Provider store={configureStore()}>
-         <Router history={browserHistory}>
-            <Route path="/" component={Root}>
-               <Route path="highscore" component={HighScore} />
-               <Route path="statistics" component={Statistics} />
-               <Route path="about" component={About} />
-            </Route>
-         </Router>
+         <IntlProvider locale={navigator.language}>
+            <Router history={browserHistory}>
+               <Route path="/" component={Root}>
+                  <Route path="highscore" component={HighScore} />
+                  <Route path="statistics" component={Statistics} />
+                  <Route path="about" component={About} />
+               </Route>
+            </Router>
+         </IntlProvider>
       </Provider>,
       document.getElementById('cornball')
    );
