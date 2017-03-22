257c2508c1704224e3bb1b55bece73495cb587c5
Victor Stodell <victor@stodell.se>
2017-03-22 12:21:58 +0100

    Added interact stubs from cornball js

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -1,19 +1,75 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import interact from 'interactjs';
 
 export default class Card extends Component {
+   constructor(props) {
+      super(props);
+
+      this.state = this.makeInitialState();
+   }
+
+   makeInitialState() {
+      return {
+         dragging: false,
+         dragX: 0,
+         dragY: 0
+      };
+   }
+
    render() {
+      const {dragging, dragX, dragY} = this.state;
       const {suit, value, roundPlaced, showHint} = this.props.card.toJS();
+
+      let className = 'card';
+      if(dragging) { className += ' dragging'; }
+      if(roundPlaced) { className += ' placed'; }
+      if(showHint) {  className += ' animated tada'; }
+
       const image = 'deck/' + suit + value + '.png';
-      const className = (roundPlaced ? 'card placed' : (showHint ? 'card animated tada' : 'card'));
+      const style = {transform: 'translate(' + dragX + 'px, ' + dragY + 'px)'};
 
       return (
-         <div className={className} onClick={this.onClick}>
+         <div className={className} style={style}>
             <img src={image} />
          </div>
       );
    }
 
-   onClick = ev => {
+   componentDidMount() {
+      const element = ReactDOM.findDOMNode(this);
+      this.interact = interact(element).draggable({
+         onstart: this.dragStart,
+         onmove: this.dragMove,
+         onend: this.dragEnd
+      }).on('doubletap', this.doubleTap);
+   }
+
+   componentWillUnmount() {
+      if(this.interact){
+         this.interact.unset();
+      }
+   }
+
+   dragStart = ev => {
+      this.setState({ dragging: true });
+   };
+
+   dragMove = ev => {
+      this.setState({
+         dragX: this.state.dragX + ev.dx,
+         dragY: this.state.dragY + ev.dy
+      });
+   };
+
+   dragEnd = ev => {
+      if(ev.dropzone === undefined) {
+         this.setState({ dragging: false });
+      }
+      this.setState(this.makeInitialState());
+   };
+
+   doubleTap = ev => {
       ev.preventDefault();
       this.props.onClick(this.props.index);
    };
--- app/components/gap.jsx
+++ app/components/gap.jsx
@@ -1,16 +1,62 @@
 import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import interact from 'interactjs';
 
 export default class Gap extends Component {
+   constructor(props) {
+      super(props);
+
+      this.state = this.makeInitialState(props);
+   }
+
+   makeInitialState(props) {
+      return {
+         highlight: false
+      };
+   }
+
    render() {
+      let className = 'gap';
+      if(this.state.highlight) { className += ' highlight'; }
+
       return (
-         <div className="gap" onClick={this.onClick} />
+         <div className={className} />
       );
    }
 
-   onClick = ev => {
+   componentDidMount() {
+      const element = ReactDOM.findDOMNode(this);
+      this.interact = interact(element).dropzone({
+         overlap: 0.1,
+         ondragenter: this.highlightGap,
+         ondragleave: this.unhighlightGap,
+         ondrop: this.cardDrop
+      }).on('tap', this.tap);
+   }
+
+   componentWillUnmount() {
+      if(this.interact){
+         this.interact.unset();
+      }
+   }
+
+   highlightGap = ev => {
+      this.setState({ highlight: true });
+   };
+
+   unhighlightGap = ev => {
+      this.setState({ highlight: false });
+   };
+
+   cardDrop = ev => {
+      //this.publishEvent('card:dropped', this.model);
+      this.unhighlightGap(ev);
+   };
+
+   tap = ev => {
       ev.preventDefault();
       this.props.onClick(this.props.index);
-   };
+   }
 }
 
 Gap.propTypes = {
