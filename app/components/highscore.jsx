5bd12014e4bf64a5067b51cda18b5d7277774ffa
Victor Stodell <victor@stodell.se>
2017-04-17 18:14:39 +0200

    Updated react and implemented proptypes

--- app/components/highscore.jsx
+++ app/components/highscore.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { FormattedMessage, FormattedDate, FormattedNumber } from 'react-intl';
 import { getHighScores, getAllTimeHigh } from '../actions/highscore';
