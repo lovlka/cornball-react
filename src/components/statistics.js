dd3cfb1f7cdd4eaf8e49b71fd68fc65b3032974e
Victor Stodell <victor@stodell.se>
2024-04-23 09:53:56 +0200

    Added default messages for statistics

--- src/components/statistics.js
+++ src/components/statistics.js
@@ -6,13 +6,26 @@ import { getStatistics } from '../helpers/selectors';
 import Modal from './modal';
 import Loader from './loader';
 
-const StatisticsRow = ({ name, value, percent }) => (
-  <tr>
-    <td><FormattedMessage id={`statistics.${name}`} /></td>
-    <td><FormattedNumber value={value} /></td>
-    <td>{percent > 0 && <FormattedNumber value={percent} style="percent" minimumFractionDigits={2} />}</td>
-  </tr>
-);
+const defaultMessages = {
+  'statistics.gamesPlayed': 'Games played',
+  'statistics.gamesLost': 'Games lost',
+  'statistics.gamesWonRound1': 'Win in round 1',
+  'statistics.gamesWonRound2': 'Win in round 2',
+  'statistics.gamesWonRound3': 'Win in round 3',
+  'statistics.gamesWonRound4': 'Win in round 4',
+  'statistics.gamesWonRound5': 'Win in round 5'
+};
+
+const StatisticsRow = ({ name, value, percent }) => {
+  const key = `statistics.${name}`;
+  return (
+    <tr>
+      <td><FormattedMessage id={key} defaultMessage={defaultMessages[key]} /></td>
+      <td><FormattedNumber value={value} /></td>
+      <td>{percent > 0 && <FormattedNumber value={percent} style="percent" minimumFractionDigits={2} />}</td>
+    </tr>
+  );
+};
 
 const Statistics = ({ onClose }) => {
   const intl = useIntl();
